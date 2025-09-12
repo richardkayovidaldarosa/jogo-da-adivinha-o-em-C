bc4570dafa44c4fce7b66c827ac940bee87a084b
richardkayovidaldarosa <richard.vidal.rosa@escola.pr.gov.br>
2025-09-12 14:44:34 +0000

    salvo

--- jogoAdivinha.c
+++ jogoAdivinha.c
@@ -4,13 +4,13 @@ int main(){
      //Imprime o cabeçalho do jogo.
     printf("***************************************\n");
     printf("#                                     #\n");
-    printf("* Bem Vindo as nosso jogo de advinhação\n");
+    printf("* Bem Vindo as nosso jogo de advinhação *\n");
     printf("[]          Bora um Frifas slk       []\n");
     printf("+                                     +\n");
     printf("---------------------------------------\n");
     
     int numeroSecreto = 58;
 
-    printf("O número secreto é")
+    printf("O número secreto é %d", numeroSecreto);
 
 }
